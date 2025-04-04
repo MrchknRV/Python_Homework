3311a7c254b68c6d11cd4a598f86e3bd7247cc69
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-04-04 09:03:29 +0300

    Added function get_mask_account for payment method

---
+++ scr/widget.py
@@ -0,0 +1,29 @@
+from typing import Any
+
+from masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(payment_method: str) -> Any:
+    """Функция возвращает строку с замаскированным номером.
+    Для карт и счетов используется разные типы маскировки"""
+    if payment_method.startswith("Счет"):
+        return (
+            payment_method[:payment_method.find(" ")]
+            + " "
+            + get_mask_account(payment_method[payment_method.find(" "):])
+        )
+    else:
+        return payment_method[:-16] + " " + get_mask_card_number(payment_method[-16:])
+
+
+# print(mask_account_card("Счет 73654108430135874305"))
+# print(mask_account_card("Maestro 1596837868705199"))
+# print(mask_account_card("Счет 64686473678894779589"))
+# print(mask_account_card("MasterCard 7158300734726758"))
+# print(mask_account_card("Visa Platinum 8990922113665229"))
+# print(mask_account_card("Visa Gold 5999414228426353"))
+# print(mask_account_card("Visa Classic 6831982476737658"))
+# print(mask_account_card("Счет 73654108430135874305"))
+
+
+
