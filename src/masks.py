f60f9dbf137d94103ef08582ead2834da54bd41d
MrchknRV <rodionmarochkin32@gmail.com>
2025-04-24 16:48:10 +0300

    fixed function in module masks

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,17 @@
 LEN_CARD_NUMBER: int = 16
+LEN_MAX_ACCOUNT_NUMBER: int = 34
 
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
 
-    if LEN_CARD_NUMBER == len(card_number):
+    if LEN_CARD_NUMBER == len(card_number) and card_number.isdigit():
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
-    else:
-        return "Вы ввели некорректный номер карты"
+    return "Вы ввели некорректный номер карты"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход номер счета и возвращает его маску."""
-    return f"**{account_number[-4:]}"
+    if 0 < len(account_number) <= 34:
+        return f"**{account_number[-4:]}"
+    return "Вы ввели некорректный номер счета"
