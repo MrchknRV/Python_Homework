9974388d3232c5b2c8a550aae9a05129ecb6a8d1
Rodion Marochkin <rodionmarochkin32@gmail.com>
2025-05-23 09:10:47 +0300

    added new func 'searchr_baking_transactions_by_string' in module proccessing

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+import re
+
+
 def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
     соответствует указанному значению."""
@@ -7,3 +10,21 @@ def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
 def sort_by_date(dict_list: list, reverse: bool = True) -> list:
     """Функция должна возвращает новый список, отсортированный по дате"""
     return sorted(dict_list, key=lambda x: x.get("date"), reverse=reverse)
+
+
+def search_banking_transactions_by_string(transactions: list, search_string=None) -> list:
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
+    а возвращает список словарей, у которых в описании есть данная строка."""
+    try:
+        if search_string is None:
+            return transactions
+        pattern = search_string
+        if len(transactions) > 0:
+            result = []
+            for trans in transactions:
+                if re.search(pattern, trans.get("description", ""), re.IGNORECASE) is not None:
+                    result.append(trans)
+            return result
+        return []
+    except Exception:
+        return []
