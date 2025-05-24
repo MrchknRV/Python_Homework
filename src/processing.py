d35d8c1d6e5695e84558ee8ddac66fde751b68bf
MrchknRV <rodionmarochkin32@gmail.com>
2025-05-24 10:59:25 +0300

    added new func 'get_count_transactions_category' in module proessing

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def filter_by_state(dict_list: list, state: str = "EXECUTED") -> list:
@@ -28,3 +29,26 @@ def search_banking_transactions_by_string(transactions: list, search_string=None
         return []
     except Exception:
         return []
+
+
+def get_count_transactions_category(transactions: list, list_categories: list) -> dict:
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций, а возвращает
+       словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
+    try:
+        if len(list_categories) == 0:
+            return {}
+
+        if len(transactions) > 0:
+            result_list = []
+            for category in list_categories:
+                pattern = f"{category}?"
+                for trans in transactions:
+                    result = re.findall(pattern, trans.get("description", ""), re.IGNORECASE)
+                    result_list = result_list + result
+                    print(result_list)
+
+            result_dict = Counter(result_list)
+            return result_dict
+        return {}
+    except Exception:
+        return {}
